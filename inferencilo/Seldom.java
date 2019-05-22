62ea48ab02477b6add01318fbab64c702b963467
Cleve Lendon <cklivo@cjki.org>
2019-05-22 19:28:31 +0900

    Seldom, to filter predicate.

---
+++ inferencilo/Seldom.java
@@ -0,0 +1,70 @@
+/**
+ * Seldom
+ *
+ * This predicate is used as a filter for rare patterns.
+ * An analysis is usually run 3 times, with Global.maxErrors equal to 0, 1, then 2.
+ * This predicate will only pass when maxErrors >= 2. Thus ensuring that
+ * other patterns are tried first.
+ *
+ * Prolog format: seldom
+ * Java format:   new Seldom()
+ *
+ * @author  Klivo
+ * @version 1.0
+ */
+
+package inferencilo;
+
+import java.util.*;
+
+public class Seldom extends BuiltInPredicate {
+
+   Constant anything = new Constant("anything");
+
+   /**
+    * constructor
+    */
+   public Seldom() {
+      super("seldom");
+   }
+
+
+   /**
+    * standardizeVariablesApart
+    *
+    * Refer to class Expression for full comments.
+    */
+   public Expression standardizeVariablesApart(Hashtable<Variable, Variable> newVars) {
+      return this;
+   }
+
+
+   /**
+    * getSolver
+    *
+    * Returns a solution node for this predicate.
+    *
+    * @param  knowledge base
+    * @param  parent solution set
+    * @param  parent solution node
+    * @return solution node
+    */
+   public SolutionNode getSolver(KnowledgeBase knowledge,
+                                 SubstitutionSet parentSolution,
+                                 SolutionNode parentNode) {
+      return new SeldomSolutionNode(this, knowledge, parentSolution, parentNode);
+   }
+
+
+   /**
+    * evaluate
+    *
+    * @param   substitution set of parent
+    * @return  a string or null
+    */
+   public Unifiable evaluate(SubstitutionSet ss) {
+      if (Global.maxErrors < 2) return null;
+      return anything;
+   }
+
+}
---
+++ inferencilo/SeldomSolutionNode.java
@@ -0,0 +1,51 @@
+/**
+ * SeldomSolutionNode
+ *
+ * Use to filter rare predicates. Pass only if Global.maxErrors >= 2.
+ *
+ * @author  Klivo
+ * @version 1.0
+ */
+
+package inferencilo;
+
+public class SeldomSolutionNode extends SolutionNode {
+
+   Seldom goal;
+   boolean moreSolutions = true;
+
+   /**
+    * constructor
+    *
+    * @param  unifiable goal
+    * @param  knowlegde base
+    * @param  parent solution set
+    * @param  parent solution node
+    * @param  solution node
+    */
+   public SeldomSolutionNode(Seldom goal, KnowledgeBase kb,
+                             SubstitutionSet parentSolution, SolutionNode parentNode) {
+      super(goal, kb, parentSolution, parentNode);
+      this.goal = goal;
+   }
+
+   /**
+    * nextSolution
+    *
+    * Call evaluate() to check if an error message can be added.
+    * If yes, evaluate will return a new error list. Unify it with
+    * the outgoing argument and return the substitution set.
+    * Fail otherwise.
+    *
+    * @return  new substitution set
+    */
+   public SubstitutionSet nextSolution() throws TimeOverrunException {
+      if (noBackChaining()) { return null; }
+      if (!moreSolutions) { return null; }
+      moreSolutions = false;
+      SubstitutionSet ss = getParentSolution();
+      Unifiable result = goal.evaluate(ss);
+      if (result == null) return null;   // failure
+      return ss;
+   }
+}
