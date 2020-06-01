082bdc89898e60509bf0b0c7c9de30aa5ebf2c89
Cleve <indriko@yahoo.com>
2020-06-01 20:50:27 +0900

    Deal with numbers.

--- inferencilo/Constant.java
+++ inferencilo/Constant.java
@@ -8,10 +8,9 @@
  * In this inference engine, a Constant holds an internal String. Integers and floats can be
  * represented internally as Strings. Arithmetic functions which operate on these Constants
  * must convert their internal strings into a numbers (if possible), execute their functions,
- * and convert the result back to a Constant.
+ * and convert the result back to a Constants.
  *
- * Reference:
- *     http://www.cs.toronto.edu/~hojjat/384w09/Lectures/Prolog_Intro.pdf
+ * Reference: http://www.cs.toronto.edu/~hojjat/384w09/Lectures/Prolog_Intro.pdf
  *
  * @author  Klivo
  * @version 1.0
@@ -20,19 +19,44 @@
 package inferencilo;
 
 import java.util.*;
+import java.util.regex.*;
 
 public class Constant implements Unifiable {
 
-   private String value = null;
+   // For detecting numbers.
+   private static Pattern numberPattern = Pattern.compile("-?\\d+(\\.\\d+)?");
 
+   private String value = null;
    /**
     * constructor
     *
-    * @param  value (string)
+    * @param  value (String)
     */
    public Constant(String value) { this.value = value; }
 
+   /**
+    * toString
+    *
+    */
    public String toString() { return value; }
+
+   /*
+    * makeDouble
+    *
+    * Makes a double floating point number from a string.
+    *
+    * @param   number as string
+    * @return   number as double
+    */
+   private double makeDouble(String strNumber) {
+      try {
+         double d = Double.parseDouble(strNumber);
+         return d;
+      } catch (NumberFormatException nfe) {
+         return 0.0;
+      }
+   }  // makeDouble
+
 
    /**
     * unify
@@ -43,6 +67,8 @@ public class Constant implements Unifiable {
     * A constant unifies with itself, i.e., 'verb = verb' succeeds.
     * To determine if two constants are the same, this method does
     * a string comparison of the internal string values.
+    * If the internal values represents numbers, unify will convert
+    * the two strings to numbers; 1 unifies with 1.000 .
     *
     * @param  other unifiable
     * @param  substitution set
@@ -50,11 +76,18 @@ public class Constant implements Unifiable {
     */
    public SubstitutionSet unify(Unifiable other, SubstitutionSet ss) {
       if (this == other) return ss;
+      if (other instanceof Variable) return other.unify(this, ss);
+      if (other instanceof Anon) return ss;
       String strThis = "" + this;
       String strThat = "" + other;
       if (strThis.equals(strThat)) return ss;
-      if (other instanceof Variable) return other.unify(this, ss);
-      if (other instanceof Anon) return ss;
+      // Check for number strings, because 1 unifies with 1.0 .
+      if (numberPattern.matcher(strThis).matches() &&
+           numberPattern.matcher(strThat).matches()) {
+           double thisFloat = makeDouble(strThis);
+           double thatFloat = makeDouble(strThat);
+           if (thisFloat == thatFloat) return ss;
+      }
       return null;
    }
 
