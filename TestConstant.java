bc8e5b11210b58feaa27d3eb7598bc328c6a0310
Cleve <indriko@yahoo.com>
2020-06-01 20:50:59 +0900

    Check unification of numbers.

--- TestConstant.java
+++ TestConstant.java
@@ -24,17 +24,28 @@ public class TestConstant {
 
       Constant test_constant = new Constant("test_constant");
       Constant red = new Constant("red");
+      Constant one = new Constant("1");
+      Constant two = new Constant("2");
+      Constant oneFloat = new Constant("1.0000");
 
-      /* Prolog equivalent: test_constant(Out) :- Out = red. */
+      /*
+        Prolog equivalent:
+                test_constant(Out) :- Out = red.
+                test_constant(Out) :- Out = 1.0, 1 = 1.0.
+                test_constant(ok) :- 1 = 1.0.
+       */
 
       KnowledgeBase kb = new KnowledgeBase(
-         new Rule(new Complex(test_constant, Out), new And( new Unify(Out, new Constant("red"))))
+         new Rule(new Complex(test_constant, Out), new And(new Unify(Out, new Constant("red")))),
+         // The next rule has no solution.
+         new Rule(new Complex(test_constant, new Constant("bad")), new And(new Unify(one, two))),
+         new Rule(new Complex(test_constant, new Constant("ok")), new And(new Unify(one, oneFloat)))
       );
 
       try {
          System.out.print("Test Constant: ");
          Complex goal = new Complex(test_constant, X);
-         String[] expected = {"red"};
+         String[] expected = {"red", "ok"};
          Solutions.verifyAll(goal, kb, expected, 1);
       }
       catch (TimeOverrunException tox) { }
