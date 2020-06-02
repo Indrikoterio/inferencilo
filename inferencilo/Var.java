bef1758c56e498cceae8d287ccfb4d51d8d46ab3
Cleve <indriko@yahoo.com>
2020-06-02 10:33:08 +0900

    Simplify. Remove Exception and set method.

--- inferencilo/Var.java
+++ inferencilo/Var.java
@@ -1,12 +1,8 @@
 /**
- * Var  (Prolog Variables)
+ * Var  (A cache of logic Variables)
  *
- * This class is a global hash of Prolog Variables.
- *
- * In this inference engine, Prolog variables can
- * be defines as so:
- *
- * Variable X = new Variable("X");
+ * This class is a hashmap of logic Variables. Variables are keyed by
+ * the variable name, eg. $X, $Y.
  *
  * @author  Klivo
  * @version 1.0
@@ -21,34 +17,22 @@ public class Var {
    private static HashMap<String, Variable> variables = new HashMap<>();
 
    /**
-    * set
-    *
-    * If the given variable already exists in the hashmap,
-    * just return it. If it doesn't yet exist, create it and return it.
-    *
-    * @param variable as string, eg: "$X"
-    * @return  Variable object.
-    */
-   public static Variable set(String str) {
-      Variable v = variables.get(str);
-      if (v != null) return v;
-      v = new Variable(str);
-      variables.put(str, v);
-      return v;
-   }
-
-   /**
     * get
+    *
+    * Gets a logic Variable from the 'variables' hashmap. If the Variable is
+    * not found in the hash, create it, store it and return it.
+    *
+    * This function is equivalent to set().
     *
     * @param variable as string
     * @return  Variable object.
-    * @throws  Illegal Argument X if variable not in hash.
     */
    public static Variable get(String str) {
       Variable v = variables.get(str);
-      if (v == null)
-         throw new IllegalArgumentException("Var.get(\"" + str + "\"): " +
-                                            "not found.");
+      if (v == null) {
+         v = new Variable(str);
+         variables.put(str, v);
+      }
       return v;
    }
 
