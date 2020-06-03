8c4075ebb60cbf294dc2f6679888dbc371aaefa9
Cleve <indriko@yahoo.com>
2020-06-04 00:04:06 +0900

    Make object vars private. Comments.

--- inferencilo/UnifySolutionNode.java
+++ inferencilo/UnifySolutionNode.java
@@ -14,9 +14,17 @@ package inferencilo;
 
 public class UnifySolutionNode extends SolutionNode {
 
-   Unify uni;
-   boolean moreSolutions = true;
+   private Unify uni;
+   private boolean moreSolutions = true;
 
+   /**
+    * constructor
+    *
+    * @param  goal
+    * @param  knowledge base
+    * @param  parent solution
+    * @param  parent node
+    */
    public UnifySolutionNode(Unify goal, KnowledgeBase kb,
                             SubstitutionSet parentSolution,
                             SolutionNode parentNode) {
@@ -41,4 +49,4 @@ public class UnifySolutionNode extends SolutionNode {
       SubstitutionSet solution = term1.unify(term2, getParentSolution());
       return solution;
    }
-}
+}  // UnifySolutionNode
