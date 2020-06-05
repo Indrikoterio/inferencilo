1ad842c97b6192b511bdc1cb81a84370a96e3eb9
Cleve <indriko@yahoo.com>
2020-06-05 09:47:01 +0900

    New tokens: LPAREN, RPAREN

--- inferencilo/TokenType.java
+++ inferencilo/TokenType.java
@@ -3,7 +3,7 @@
  *
  * For parsing the goal of a Prolog rule.
  * For the goal: father($X, $Z), parent($Z, $Y)
- * the token types will be: TERM, COMMA, TERM
+ * the token types will be: TERM, COMMA, TERM, LPAREN, RPAREN.
  *   
  * @author  Cleve (Klivo) Lendon
  * @version 1.0
@@ -12,7 +12,7 @@
 package inferencilo;
 
 public enum TokenType {
-   TERM, COMMA, SEMICOLON;
+   TERM, COMMA, SEMICOLON, LPAREN, RPAREN;
 }  // TokenType
 
 
