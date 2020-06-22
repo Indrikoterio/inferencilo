756eeb7b7359eeb39bc1d7fb2d015198c83522cb
Cleve <indriko@yahoo.com>
2020-06-22 18:11:00 +0900

    Improved comments.

--- inferencilo/ComplexSolutionNode.java
+++ inferencilo/ComplexSolutionNode.java
@@ -1,6 +1,21 @@
 /**
  * ComplexSolutionNode
  *
+ * Solution node for Complex terms (= compound terms).
+ *
+ * This node has a child node, which is the next subgoal. The method
+ * nextSolution() will check to see if the child has a solution.
+ * If it does, this solution (substitution set) is returned.
+ *
+ * Otherwise, nextSolution() fetches rules/facts from the knowledge
+ * base, and tries to unify the head of these rules with the goal.
+ * If a matching fact is found, and then the solution is returned.
+ * (Note, a fact is a rule without a body.)
+ *
+ * Otherwise, the body node of the rule becomes the child node, and
+ * the algorithm tries to find a solution (substitution set) for the
+ * child. It will return the child solution or null for failure.
+ *
  * @author  Klivo
  * @version 1.0
  */
@@ -11,6 +26,14 @@ public class ComplexSolutionNode extends SolutionNode {
 
    private SolutionNode child = null;
 
+   /**
+    * constructor
+    *
+    * @param  goal
+    * @param  knowledge base
+    * @param  parent solution (substitution set)
+    * @param  parent solution node
+    */
    public ComplexSolutionNode(Complex goal,
                               KnowledgeBase knowledge,
                               SubstitutionSet parentSolution,
@@ -20,6 +43,12 @@ public class ComplexSolutionNode extends SolutionNode {
    }
 
 
+   /**
+    * nextSolution
+    *
+    *
+    * @throws TimeOverrunException
+    */
    public SubstitutionSet nextSolution() throws TimeOverrunException {
 
       if (noBackChaining()) { return null; }
@@ -40,9 +69,9 @@ public class ComplexSolutionNode extends SolutionNode {
          solution = head.unify((Unifiable)goal, getParentSolution());
 
          if (solution != null) {
-            Goal tail = rule.getBody();
-            if (tail == null) return solution;
-            child = tail.getSolver(getKnowledgeBase(), solution, this);
+            Goal body = rule.getBody();
+            if (body == null) return solution;
+            child = body.getSolver(getKnowledgeBase(), solution, this);
             SubstitutionSet childSolution = child.nextSolution();
             if (childSolution != null) return childSolution;
          }
@@ -50,7 +79,13 @@ public class ComplexSolutionNode extends SolutionNode {
       return null;
    }
 
+   /**
+    * getChild
+    *
+    * @return child
+    */
    public SolutionNode getChild() {
       return child;
    }
-}
+
+} // ComplexSolutionNode
