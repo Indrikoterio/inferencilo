e7e2d4e27a4a75fa04e8bfa523e75bdb435b7aa2
Cleve <indriko@yahoo.com>
2020-06-22 16:42:20 +0900

    Better comments. Remove unused rules.

--- inferencilo/SolutionNode.java
+++ inferencilo/SolutionNode.java
@@ -1,7 +1,18 @@
 /**
  * SolutionNode
  *
- * @author  Klivo
+ * Represents a node in a 'proof tree'.
+ *
+ * Complex terms and operators (And, Or, Unify, etc.) implement a
+ * method called getSolver(), which returns a SolutionNode specific
+ * to the term or operator.
+ *
+ * The method nextSolution() starts the search for a solution.
+ * When a solution is found, the search stops. Each node preserves
+ * its state (goal, ruleNumber, etc.). Calling nextSolution() again
+ * will continue the search from where it left off.
+ *
+ * @author  Cleve (Klivo) Lendon
  * @version 1.0
  */
 
@@ -18,7 +29,7 @@ public abstract class SolutionNode {
 
    private Rule   currentRule = null;
    Goal   goal = null;     // goal being solved
-   String predicateName;   // Use as key to kb, eg.  mother/2
+   String predicateName;   // Use as key to kb, eg. mother/2
 
    /**
     * constructor
@@ -39,18 +50,23 @@ public abstract class SolutionNode {
       this.parentNode     = parentNode;
    }
 
+   /**
+    * nextSolution
+    *
+    * Abstract method to initiate or continue the search for a solution.
+    *
+    * @return substitution set
+    * @throws TimeOverrunException
+    */
    public abstract SubstitutionSet nextSolution() throws TimeOverrunException;
 
-   public void reset(SubstitutionSet newParentSolution) {
-      parentSolution = newParentSolution;
-      ruleNumber = 0;
-   }
-
-
-   /*
+   /**
     * nextRule
     *
-    * Fetches next rule from the database.
+    * Fetches next rule from the database, with standardized variables.
+    * Returns null (failure) if the goal has been exhausted.
+    *
+    * @return rule object
     */
    public Rule nextRule() {
       if (hasNextRule()) {
@@ -62,37 +78,63 @@ public abstract class SolutionNode {
       return currentRule;
    }
 
+   /*
+    * hasNextRule()
+    *
+    * Returns true if the knowledge base contains untried rules
+    * for this node's goal. False otherwise.
+    *
+    * @return t/f
+    */
    protected boolean hasNextRule() {
       return ruleNumber < knowledge.getRuleCount(goal);
    }
 
+   /*
+    * getParentSolution
+    *
+    * Returns parent's solution (substitution set).
+    *
+    * @return substitution set
+    */
    protected SubstitutionSet getParentSolution() {
       return parentSolution;
    }
 
+   /*
+    * getKnowledgeBase
+    *
+    * @return knowledge base
+    */
    protected KnowledgeBase getKnowledgeBase() {
       return knowledge;
    }
 
-   public Rule getCurrentRule() {
-      return currentRule;
-   }
+   /**
+    * getParentNode
+    *
+    * @return parent solution node
+    */
+   public SolutionNode getParentNode() { return parentNode; }
 
-   public Goal getGoal() {
-      return goal;
-   }
-
-   public SolutionNode getParentNode() {
-      return parentNode;
-   }
-
+   /**
+    * setNoBackChaining
+    *
+    * Disables back chaining. This is used for the cut operator: !.
+    */
    public void setNoBackChaining() {
       noBackChaining = true;
    }
 
+   /**
+    * noBackChaining
+    *
+    * Tests whether back chaining is allowed or not.
+    *
+    * @return t/f
+    */
    public boolean noBackChaining() {
       return noBackChaining;
    }
 
-
-}
+}  // SolutionNode
