d25c7ada1c8f3145ad4696c83d33be2c36304550
Cleve <indriko@yahoo.com>
2020-06-25 11:51:05 +0900

    Comments. x, y -> X, Y

--- TestFunctor.java
+++ TestFunctor.java
@@ -3,7 +3,7 @@
  *
  * Test the Functor function.
  *
- * @author  Klivo
+ * @author  Cleve (Klivo) Lendon
  * @version 1.0
  */
 
@@ -14,18 +14,18 @@ public class TestFunctor {
 
    public static void main(String[] args) {   // Set up the knowledge base.
 
-      Variable x   = new Variable("$X");
-      Variable y   = new Variable("$Y");
+      Variable X   = new Variable("$X");
+      Variable Y   = new Variable("$Y");
       Constant get = new Constant("get");
       Complex  term = new Complex("mouse(mammal, rodent)");
 
       KnowledgeBase kb = new KnowledgeBase(
-         new Rule(new Complex(get, y),
+         new Rule(new Complex(get, Y),
             new And(
                new Unify(
-                  x, new Functor(term)
+                  X, new Functor(term)
                ),
-               new Unify(x, y)
+               new Unify(X, Y)
             )
          )
       );
@@ -34,7 +34,7 @@ public class TestFunctor {
       System.out.print("Test Functor: ");
 
       try {
-         Complex goal = new Complex(get, x);
+         Complex goal = new Complex(get, X);
          String[] expected = {"mouse"};
          Solutions.verifyAll(goal, kb, expected, 1);
       }
