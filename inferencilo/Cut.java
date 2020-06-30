1b2c425c9a20423dd51d48346de5ad46b8093e31
Cleve <indriko@yahoo.com>
2020-06-30 23:41:07 +0900

    Comments.

--- inferencilo/Cut.java
+++ inferencilo/Cut.java
@@ -3,29 +3,43 @@
  *
  * Defines logical 'Cut' operator.
  *
- * @author  Klivo
+ * @author  Cleve (Klivo) Lendon
  * @version 1.0
  */
 
 package inferencilo;
 
-import java.util.*;
-
 public class Cut extends Operator implements Goal {
 
+   /**
+    * constructor
+    */
    public Cut() { }
 
+   /**
+    * toString
+    */
    public String toString() { return " Cut "; }
 
+   /**
+    * getSolver
+    *
+    * Returns a solution node.
+    *
+    * @param  knowledge base
+    * @param  parent solution set
+    * @param  parent solution node
+    * @return solution node
+    */
    public SolutionNode getSolver(KnowledgeBase knowledge,
-                                         SubstitutionSet parentSolution,
-                                         SolutionNode parentNode) {
+                                 SubstitutionSet parentSolution,
+                                 SolutionNode parentNode) {
       return new CutSolutionNode(this, knowledge, parentSolution, parentNode);
    }
 
    // getCopy, to avoid cloning.
    public Operator getCopy() {
       return new Cut();
-   };
+   }
 
-}
+} // Cut
