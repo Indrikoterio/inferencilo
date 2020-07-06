f1309d119cf1ec6907f141913293bdd1860a6dbe
Cleve <indriko@yahoo.com>
2020-07-06 17:30:52 +0900

    New functions castConstant, castComplex, castPList.

--- inferencilo/SubstitutionSet.java
+++ inferencilo/SubstitutionSet.java
@@ -1,7 +1,17 @@
 /**
  * SubstitutionSet
  *
- * A dictionary (hash) of bindings of logic Variables.
+ * The substitution set is a dictionary (aka hashmap) of bindings
+ * of logic Variables.
+ *
+ * As the inference engine searches for a solution, it adds Variable
+ * bindings to the SubstitutionSet. Because the SubstitutionSet
+ * contains all Variable bindings, it can be thought of as the
+ * solution (partial or final).
+ *
+ * This class also has methods to get the ground term of a
+ * Variable, and cast it as a Constant, Complex term, or PList.
+ * See castConstant(), castComplex(), castPList().
  *
  * @author  Cleve (Klivo) Lendon
  * @version 1.0
@@ -119,5 +129,76 @@ public class SubstitutionSet {
 
    public String toString() { return "Bindings: (" + bindings + ")"; }
 
-}
+
+   /**
+    * castConstant
+    *
+    * If the given unifiable is an instance of Constant, cast it
+    * as Constant and return it. Otherwise return null.
+    *
+    * @param  Unifiable term
+    * @return Constant term or null
+    */
+   public Constant castConstant(Unifiable term) {
+      if (term instanceof Constant) return (Constant)term;
+      Unifiable outTerm = null;
+      if (term instanceof Variable) {
+         if (isGround((Variable)term)) {
+            outTerm = getGroundTerm((Variable)term);
+         }
+         else return null;
+      }
+      if (outTerm instanceof Constant) return (Constant)outTerm;
+      return null;
+   } // castConstant()
+
+
+   /**
+    * castComplex
+    *
+    * If the given unifiable is an instance of Complex, cast it
+    * as complex. Otherwise return null.
+    *
+    * @param  Unifiable term
+    * @return Complex term or null
+    */
+   public Complex castComplex(Unifiable term) {
+      if (term instanceof Complex) return (Complex)term;
+      Unifiable outTerm = null;
+      if (term instanceof Variable) {
+         if (isGround((Variable)term)) {
+            outTerm = getGroundTerm((Variable)term);
+         }
+         else return null;
+      }
+      if (outTerm instanceof Complex) return (Complex)outTerm;
+      return null;
+   } // castComplex()
+
+
+   /**
+    * castPList
+    *
+    * If the given unifiable is an instance of PList, cast it
+    * as PList and return it. If it is a Variable, get the term
+    * which it is bound to. If that term is a PList, cast it as
+    * a PList and return it. Otherwise return null.
+    *
+    * @param  unifiable term
+    * @param  plist or null
+    */
+   public PList castPList(Unifiable term) {
+      if (term instanceof PList) return (PList)term;
+      Unifiable outTerm = null;
+      if (term instanceof Variable) {
+         if (isGround((Variable)term)) {
+            outTerm = getGroundTerm((Variable)term);
+         }
+         else return null;
+      }
+      if (outTerm instanceof PList) return (PList)outTerm;
+      return null;
+   } // castPList()
+
+} // SubstitutionSet
 
