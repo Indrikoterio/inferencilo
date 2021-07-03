68acecbd13798483267be00084960cc2233f7bd0
Cleve <indriko@yahoo.com>
2021-07-03 16:33:56 +0900

    Tests the Join function.

---
+++ TestJoin.java
@@ -0,0 +1,47 @@
+/**
+ * TestJoin
+ *
+ * Test the built-in function join().
+ * This function joins constants, representing a list words
+ * and punctuation, to form a single constant.
+ *
+ * @author  Cleve (Klivo) Lendon
+ * @version 1.0
+ */
+
+import inferencilo.*;
+
+public class TestJoin {
+
+   public static void main(String[] args) {
+
+      Variable D1 = Variable.instance("$D1");
+      Variable D2 = Variable.instance("$D2");
+      Variable D3 = Variable.instance("$D3");
+      Variable D4 = Variable.instance("$D4");
+      Variable D5 = Variable.instance("$D5");
+      Variable Out = Variable.instance("$Out");
+
+      KnowledgeBase kb = new KnowledgeBase(
+         new Rule(new Complex("would_you_like($Out)"),
+            new And(
+                   new Unify(D1, new Constant("coffee")),
+                   new Unify(D2, new Constant(",")),
+                   new Unify(D3, new Constant("tea")),
+                   new Unify(D4, new Constant("or")),
+                   new Unify(D5, new Constant("juice")),
+                   new Unify(Out, new Join(D1, D2, D3, D4, D5))
+                )
+         )
+      );
+
+      System.out.print("Test Join: ");
+
+      try {
+         Complex goal = new Complex("would_you_like($X)");
+         String[] expected = { "coffee, tea or juice" };
+         Solutions.verifyAll(goal, kb, expected, 1);
+      } catch (TimeOverrunException tox) {}
+   }
+
+} // TestJoin
