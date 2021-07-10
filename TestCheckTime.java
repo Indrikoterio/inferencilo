a1d6fe8842f81de0d4eaaa68e0bdb034eabccb56
Cleve <indriko@yahoo.com>
2021-07-10 14:03:06 +0900

    Test the CheckTime predicate.

---
+++ TestCheckTime.java
@@ -0,0 +1,78 @@
+/**
+ * TestCheckTime
+ *
+ * Testing the CheckTime predicate, using an infinite loop.
+ *
+ * fact(a,b).
+ * fact(b,c).
+ * doit(X,Z) :- fact(X,Z).
+ * doit(X,Z) :- print("."), doit(X,Y), fact(Y,Z), check_time.
+ *
+ * Note: It is necessary to set startTime before searching
+ * for a solution.
+ *
+ * The global variable maxTime is 300 milliseconds by default,
+ * but here it is set to 10 milliseconds.
+ *
+ * @author  Klivo
+ * @version 1.0
+ */
+
+import java.util.*;
+import inferencilo.*;
+
+public class TestCheckTime {
+
+   public static void main(String[] args) {   // Set up the knowledge base.
+
+      Constant a = new Constant("a");
+      Constant b = new Constant("b");
+      Constant c = new Constant("c");
+
+      Variable X = Variable.instance("$X");
+      Variable Y = Variable.instance("$Y");
+      Variable Z = Variable.instance("$Z");
+
+      Constant fact = new Constant("fact");
+      Constant doit = new Constant("doit");
+
+      KnowledgeBase kb = new KnowledgeBase(
+
+         new Rule(new Complex(fact, a, b)),
+         new Rule(new Complex(fact, b, c)),
+
+         new Rule(
+            new Complex(doit, X, Z),
+            new And( new Complex(fact, X, Z) )
+         ),
+
+         new Rule(
+            new Complex(doit, X, Z),
+            new And(
+               new Print("."),
+               new Complex(doit, X, Y),
+               new Complex(fact, Y, Z),
+               new CheckTime()
+            )
+         )
+
+      );
+
+      System.out.print("Test CheckTime: ");
+
+      Global.maxTime = 10;  // milliseconds
+      Global.startTime = System.nanoTime();
+
+      try {
+         Complex goal = new Complex("doit($_, Y)");
+         String[] expected = { "something" };
+         Solutions.verifyAll(goal, kb, expected, 2);
+      } catch (TimeOverrunException tox) {
+         System.err.println("Time out. ✓");
+      }
+
+   }
+
+} // TestCheckTime
+
+
