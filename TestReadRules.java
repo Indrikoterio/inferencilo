d38ed7818b3c8979e6bc0c07ef6b4c291df5786f
Cleve <indriko@yahoo.com>
2021-07-13 21:20:37 +0900

    Test for ReadRules. Read kings.txt .

---
+++ TestReadRules.java
@@ -0,0 +1,33 @@
+/**
+ * TestReadRules
+ *
+ * Read in the rules from a text file (kings.txt).
+ * Who is Skule's grandfather.
+ *
+ * @author  Cleve (Klivo) Lendon
+ * @version 1.0
+ */
+
+import java.util.*;
+import inferencilo.*;
+
+public class TestReadRules {
+
+   public static void main(String[] args) {
+
+      KnowledgeBase kb = new KnowledgeBase();
+      List<String> reguloj = ReadRules.fromFile("kings.txt");
+      kb.addRules(reguloj);
+      //kb.showKB();
+
+      System.out.print("Test ReadRules: ");
+
+      try {
+         Complex goal = new Complex("grandfather($X, Skule)");
+         String[] expected = { "Godwin" };
+         Solutions.verifyAll(goal, kb, expected, 1);
+      } catch (TimeOverrunException tox) {}
+
+   }
+
+} // TestReadRules
