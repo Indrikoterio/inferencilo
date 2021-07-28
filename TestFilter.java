f08948681cc055d27d5db50d7aadde932087d0ce
Cleve <indriko@yahoo.com>
2021-07-28 11:11:26 +0900

    Test for nerds.

--- TestFilter.java
+++ TestFilter.java
@@ -15,6 +15,7 @@
  * The filter test:
  *
  * list_wimmin($W) :- friends($F), filter($F, female, $W).")
+ * list_nerds($N)  :- friends($F), filter($F, male, $N).")
  * 
  * @author  Cleve (Klivo) Lendon
  * @version 1.0
@@ -35,15 +36,20 @@ public class TestFilter {
          new Rule("female(Bernadette)"),
          new Rule("female(Amy)"),
          new Rule("friends([Leonard, Raj, Penny, Sheldon, Bernadette, Amy, Howard])."),
-         new Rule("list_wimmin($W) :- friends($F), filter($F, female, $W).")
+         new Rule("list_wimmin($W) :- friends($F), filter($F, female, $W)."),
+         new Rule("list_nerds($N) :- friends($F), filter($F, male, $N).")
       );
 
       System.out.print("Test Filter function: ");
 
       try {
-         Complex goal = new Complex("list_wimmin($F)");
+         Complex goal = new Complex("list_wimmin($W)");
          String[] expected = { "[Penny, Bernadette, Amy]" };
          Solutions.verifyAll(goal, kb, expected, 1);
+
+         goal = new Complex("list_nerds($N)");
+         String[] expected2 = { "[Leonard, Raj, Sheldon, Howard]" };
+         Solutions.verifyAll(goal, kb, expected2, 1);
       } catch (TimeOverrunException tox) {}
    }
 
