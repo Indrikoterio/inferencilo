ddf5927e08dd72f7dd962b1bf606858c4e365f5e
Cleve <indriko@yahoo.com>
2021-08-01 02:49:04 +0900

    Function to test PFunction class.

---
+++ test/Capitalize.java
@@ -0,0 +1,67 @@
+/**
+ * Capitalize
+ *
+ * This function capitalizes a word or name.
+ *
+ * It accepts one argument, a Constant or Variable bound to a Constant,
+ * and returns a capitalized Constant. (tokyo becomes Tokyo.)
+ *
+ * Note: Unlike a predicate, which returns a substitution set (or null),
+ * a function returns a Unifiable term. Therefore, it must be used with
+ * unification (=). Eg.
+ *
+ *    ..., $CapName = capitalize($Name),...
+ *
+ * @author  Cleve (Klivo) Lendon
+ * @version 1.0
+ */
+
+import inferencilo.*;
+
+public class Capitalize extends PFunction {
+
+   /**
+    * constructor
+    *
+    * @param  unifiable arguments
+    * @throws  TooFewArgumentsException, TooManyArgumentsException
+    */
+   public Capitalize(Unifiable... args) {
+      super("Capitalize", args);
+      if (arguments.length < 1) throw new TooFewArgumentsException("- Capitalize");
+      if (arguments.length > 1) throw new TooManyArgumentsException("- Capitalize");
+   }
+
+   /**
+    * constructor
+    *
+    * @param  arguments as string
+    * @throws  TooFewArgumentsException, TooManyArgumentsException
+    */
+   public Capitalize(String str) {
+      super("Capitalize", str);
+      if (arguments.length < 1) throw new TooFewArgumentsException("- Capitalize");
+      if (arguments.length > 1) throw new TooManyArgumentsException("- Capitalize");
+   }
+
+   /**
+    * evaluate the arguments
+    *
+    * @param   Substitution Set
+    * @param   Unifiable parameters
+    * @return  Constant
+    */
+   public Unifiable evaluate(SubstitutionSet ss, Unifiable... params) {
+
+      Constant c = ss.castConstant(arguments[0]);
+      if (c == null) return new Constant("?!");
+      if (c.toString().length() == 0) return new Constant("");
+
+      String first = c.toString().substring(0, 1).toUpperCase();
+      String theRest = c.toString().substring(1);
+
+      return new Constant(first + theRest);
+
+   } // evaluate()
+
+}  // Capitalize
