e7a4d162131c570e5d314f4bbcbc0fab4bbcf8d3
Cleve <indriko@yahoo.com>
2021-08-01 12:15:30 +0900

    Separate two tests. Ouput better report message.

--- test/TestPrint.java
+++ test/TestPrint.java
@@ -30,16 +30,26 @@ public class TestPrint {
          )
       );
 
-      Rule rule = new Rule("print_test :- $X = World, $Y = Cleve, " +
-                           "print(\\nHello %s\\, my name is %s., $X, $Y), nl.");
+      Rule rule = new Rule("print_test2 :- $X = World, $Y = Cleve, " +
+                           "print(Hello %s\\, my name is %s., $X, $Y), nl.");
       kb.addRule(rule);
 
-      System.out.println("Test Print");
+      System.out.println("Test Print 2 - should print:");
+      System.out.println("Persian, king, [Cyrus, Cambysis, Darius]");
 
       try {
          // Define goal and root of search space.
          Complex goal = new Complex("print_test");
-         String[] expected = {"print_test", "print_test"};
+         String[] expected = {"print_test"};
+         Solutions.verifyAll(goal, kb, expected, 0);
+      } catch (TimeOverrunException tox) { }
+
+      System.out.println("Test Print - should print:");
+      System.out.println("Hello World, my name is Cleve.");
+
+      try {
+         Complex goal = new Complex("print_test2");
+         String[] expected = {"print_test2"};
          Solutions.verifyAll(goal, kb, expected, 0);
       } catch (TimeOverrunException tox) { }
    }
