7988e86eb0d6d9ee2527018a409d0047cb4b93d7
Cleve <indriko@yahoo.com>
2021-08-07 20:10:19 +0900

    New constructor, list of strings.

--- inferencilo/CompareBase.java
+++ inferencilo/CompareBase.java
@@ -30,7 +30,35 @@ public abstract class CompareBase extends BuiltInPredicate {
    /**
     * constructor
     *
+    * This constructor takes a list of strings which represent the
+    * arguments of the predicate. For example, if the predicate is
+    *     greater_than($X, 10),
+    * ...the list will have "$X" and "10".
+    *
+    * @param  name of predicate
+    * @param  argument list
+    * @throws TooManyArgumentsException, TooFewArgumentsException
+    */
+   public CompareBase(String name, List<String> args) {
+
+      super(name);
+      int size = args.size();
+      if (size > 2) throw new TooManyArgumentsException("- " + name);
+      if (size < 2) throw new TooFewArgumentsException("- " + name);
+
+      this.arguments = args.stream().map(Make::term)
+                                    .toArray(Unifiable[]::new);
+   }  // constructor
+
+
+   /**
+    * constructor
+    *
     * This constructor takes arguments in string form.
+    * The two arguments are separated by a comma.
+    *
+    * For the predicate greater_than($X, 10),
+    * ...the argument string is: "$X, 10"
     *
     * @param  name of predicate
     * @param  argument string
