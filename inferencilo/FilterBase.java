78567b43afb4ed81434a3b28c2dd39bd96359b57
Cleve <indriko@yahoo.com>
2021-08-21 13:21:41 +0900

    No need to test class of term. PList.class.isInstante(term)

--- inferencilo/FilterBase.java
+++ inferencilo/FilterBase.java
@@ -166,7 +166,7 @@ public abstract class FilterBase extends BuiltInPredicate {
          if (pList.isTailVar() && !Anon.class.isInstance(head)) {
             Variable hVar  = (Variable)(head);
             PList term = ss.castPList(hVar);
-            if (term != null && PList.class.isInstance(term)) {
+            if (term != null) {
                pList = (PList)term;
                head = pList.getHead();
             }
