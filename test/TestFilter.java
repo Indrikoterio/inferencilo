6fb66a3d07b04115977bf60f2a8b37828e28fddd
Cleve <indriko@yahoo.com>
2022-05-03 15:39:29 +0900

    Standardize include(), exclude(). First argument is goal.

--- test/TestFilter.java
+++ test/TestFilter.java
@@ -1,23 +1,12 @@
 /**
  * TestFilter
  *
- * Test the include and exclude predicates.
+ * Test the include() and exclude() predicates. Eg.
  *
- * First, the facts:
- *
- * male(Sheldon).
- * male(Leonard).
- * male(Raj).
- * male(Howard).
- * female(Penny).
- * female(Bernadette).
- * female(Amy).
- * friends([Leonard, Raj, Penny, Sheldon, Bernadette, Amy, Howard]).
- *
- * The filter test:
- *
- * list_wimmin($W) :- friends($F), include(female, $F, $W).
- * list_nerds($N)  :- friends($F), exclude(female, $F, $N).
+ * $People = [male(Sheldon), male(Leonard), male(Raj), male(Howard),
+ *            female(Penny), female(Bernadette), female(Amy)]
+ * list_wimmin($W) :- include(female($_), $People, $W).
+ * list_nerds($N)  :- exclude(female($_), $People, $N).
  * 
  * @author  Cleve (Klivo) Lendon
  * @version 1.0
@@ -29,28 +18,39 @@ public class TestFilter {
 
    public static void main(String[] args) {
 
+      Variable W = new Variable("$W");
+      Variable N = new Variable("$N");
+
+      Complex c1 = new Complex("male(Sheldon)");
+      Complex c2 = new Complex("male(Leonard)");
+      Complex c3 = new Complex("male(Raj)");
+      Complex c4 = new Complex("male(Howard)");
+      Complex c5 = new Complex("female(Penny)");
+      Complex c6 = new Complex("female(Bernadette)");
+      Complex c7 = new Complex("female(Amy)");
+      PList people = new PList(false, c1, c2, c3, c4, c5, c6, c7);
+
+      Complex list_wimmin = new Complex("list_wimmin($W)");
+      Complex list_nerds  = new Complex("list_nerds($N)");
+      Complex filter      = new Complex("female($_)");
+
+      Include inc = new Include(filter, people, W);
+      Exclude ex  = new Exclude(filter, people, N);
+
       KnowledgeBase kb = new KnowledgeBase(
-         new Rule("male(Sheldon)"),
-         new Rule("male(Leonard)"),
-         new Rule("male(Raj)"),
-         new Rule("male(Howard)"),
-         new Rule("female(Penny)"),
-         new Rule("female(Bernadette)"),
-         new Rule("female(Amy)"),
-         new Rule("friends([Leonard, Raj, Penny, Sheldon, Bernadette, Amy, Howard])."),
-         new Rule("list_wimmin($W) :- friends($F), include(female, $F, $W)."),
-         new Rule("list_nerds($N) :- friends($F), exclude(female, $F, $N).")
+         new Rule(list_wimmin, inc),
+         new Rule(list_nerds, ex)
       );
 
-      System.out.print("Test Include and Exclude predicates: ");
+      System.out.println("Test Include and Exclude predicates: ");
 
       try {
          Complex goal = Make.goal("list_wimmin($W)");
-         String[] expected = { "[Penny, Bernadette, Amy]" };
+         String[] expected = { "[female(Penny), female(Bernadette), female(Amy)]" };
          Solutions.verifyAll(goal, kb, expected, 1);
 
          goal = Make.goal("list_nerds($N)");
-         String[] expected2 = { "[Leonard, Raj, Sheldon, Howard]" };
+         String[] expected2 = { "[male(Sheldon), male(Leonard), male(Raj), male(Howard)]" };
          Solutions.verifyAll(goal, kb, expected2, 1);
       } catch (TimeOverrunException tox) {}
    }
