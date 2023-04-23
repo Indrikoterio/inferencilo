43f221e142189432501118cb6024a67d2a332012
Cleve <indriko@yahoo.com>
2023-04-23 12:37:41 +0900

    Add question mark to test.

--- test/TestJoin.java
+++ test/TestJoin.java
@@ -1,12 +1,25 @@
 /**
- * TestJoin
+ * TestJoin - Test the built-in function join().
  *
- * Test the built-in function join().
  * This function joins constants, representing a list words
  * and punctuation, to form a single constant.
  *
+ * In the following source example,
+ *
+ *   $D1 = coffee, $D2 = "," , $D3 = tea,
+ *   $D4 = or, $D5 = juice, $D6 = ?,
+ *   $X = join($D1, $D2, $D3, $D4, $D5, $D6).
+ *
+ * $X is bound to the Atom "coffee, tea or juice?".
+ *
+ * A built-in function is different from a built-in predicate, in
+ * that a built-in function returns a value which must be unified
+ * with something in order to be useful. All the arguments of
+ * a function must be constants or grounded variables. If not,
+ * the function fails.
+ *
  * @author  Cleve (Klivo) Lendon
- * @version 1.0
+ * @version 1.1
  */
 
 import inferencilo.*;
@@ -20,6 +33,7 @@ public class TestJoin {
       LogicVar D3 = new LogicVar("$D3");
       LogicVar D4 = new LogicVar("$D4");
       LogicVar D5 = new LogicVar("$D5");
+      LogicVar D6 = new LogicVar("$D6");
       LogicVar Out = new LogicVar("$Out");
 
       KnowledgeBase kb = new KnowledgeBase(
@@ -30,7 +44,8 @@ public class TestJoin {
                    new Unify(D3, new Constant("tea")),
                    new Unify(D4, new Constant("or")),
                    new Unify(D5, new Constant("juice")),
-                   new Unify(Out, new Join(D1, D2, D3, D4, D5))
+                   new Unify(D6, new Constant("?")),
+                   new Unify(Out, new Join(D1, D2, D3, D4, D5, D6))
                 )
          )
       );
@@ -39,27 +54,27 @@ public class TestJoin {
 
       try {
          Complex query = Make.query("would_you_like($X)");
-         String[] expected = { "coffee, tea or juice" };
+         String[] expected = { "coffee, tea or juice?" };
          Solutions.verifyAll(query, kb, expected, 1);
       } catch (TimeOverrunException tox) {}
 
-      // New test.
-      // Let the parser create a join function.
+      //--------------------------------------------------
+      // New test. Let the parser create a join function.
 
       kb = new KnowledgeBase();
       String str = "would_you_like($Out) :- $D1 = coffee, $D2 = \\,, " +
-                   "$D3 = tea, $D4 = or, $D5 = juice, " +
-                   "$Out = join($D1, $D2, $D3, $D4, $D5).";
+                   "$D3 = tea, $D4 = or, $D5 = juice, $D6 = ?, " +
+                   "$Out = join($D1, $D2, $D3, $D4, $D5, $D6).";
 
       Rule rule = new Rule(str);
       kb.addRule(rule);
 
       try {
          Complex query = Make.query("would_you_like($X)");
-         String[] expected = { "coffee, tea or juice" };
+         String[] expected = { "coffee, tea or juice?" };
          Solutions.verifyAll(query, kb, expected, 1);
       } catch (TimeOverrunException tox) {}
 
-   }
+   } // main
 
 } // TestJoin
